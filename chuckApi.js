2babf88f6fb6da83d500f4423498117b471cb6d5
vcelis <vincent.celis@gmail.com>
2014-06-12 19:26:47 +0200

    working prototype v0.0.1

---
+++ chuckApi.js
@@ -0,0 +1,37 @@
+var http = require('http');
+
+function ChuckApi(config) {
+  this.config = {
+      host: config.apiHostname,
+      port: config.apiPort,
+      path: '/jokes/random',
+      method: 'GET'
+  };
+}
+
+ChuckApi.prototype = {
+  callApi: function(cb) {
+    var chuckApi = this;
+
+    http.request(chuckApi.config, function(res) {
+      var code = res.statusCode;
+      var result = '';
+      code = res.statusCode;
+
+      if ( code === 200 ) {
+        res.setEncoding('utf8');
+        
+        res.on('data', function(data) {
+          result += data;
+        });
+        
+        res.on('end', function() {
+          cb(JSON.parse(result).value.joke);
+        });
+      }
+
+    }).end();
+  }
+};
+
+exports.ChuckApi = ChuckApi;
--- chuckie.js
+++ chuckie.js
@@ -1,67 +1,48 @@
-var http = require('http');
-var callApi = function(path, callback) {
-  var options = {
-    host: 'api.icndb.com',
-    port: 80,
-    path: path,
-    method: 'GET'
-  };
-  http.request(options, function(res) {
-    var code = res.statusCode;
-    var result = '';
-    code = res.statusCode;
-    if ( code === 200 ) {
-      res.setEncoding('utf8');
-      res.on('data', function(data) {
-        result += data;
-      });
-      res.on('end', function() {
-        callback(JSON.parse(result));
-      });
-    }
-  }).end();
-};
-
-var getJoke = function() {
-  callApi('/jokes/random', function(joke) {
-    return joke.value.joke;
-  });
-};
-
-
 var irc = require('irc');
+var chuckApi = require('./chuckApi.js');
 
 function Chuckie(config) {
   this.config = config;
   this.client = new irc.Client(this.config.server, this.config.userName, this.config);
+  this.chuckApi = new chuckApi.ChuckApi(config);
 }
 
-Chuckie.prototype.addListeners = function() {
-  var chuckie = this;
-  this.client.addListener('message', function(from, to, text, message) {
-    var regex = new RegExp('^' + chuckie.config.userName + '[,:\s\\|].*$', 'i');
 
-    if ( to === chuckie.config.userName || text.match(regex) ) {
-      chuckie.client.say(message.args[0], 'something funny here');
-    } 
-  });
-}
 
-Chuckie.prototype.start = function() {
-  var chuckie = this;
-  console.log('Connecting to IRC...');
-  try {
-    chuckie.client.connect(10);
-    console.log('Succesfully connected!');
-    chuckie.addListeners();
-    console.log('Succesfully added listeners');
-  } catch (ex) {
-    console.log('Some error occured while connecting:');
-    console.log(ex);
-    console.log('-------------------------------------');
-    console.log('Retry in 5 seconds');
-    setTimeout(function() { this.start() }, 2000);
+Chuckie.prototype = {
+
+  start: function() {
+    var chuckie = this;
+
+    console.log('Bot is starting');
+    try {
+      chuckie.client.connect(10);
+      console.log('Succesfully connected!');
+      chuckie.addListeners();
+      console.log('Succesfully added listeners');
+    } catch (ex) {
+      console.log('Some error occured while connecting:');
+      console.log(ex);
+      console.log('-------------------------------------');
+      console.log('Retry in 5 seconds');
+      setTimeout(function() { this.start() }, 2000);
+    }
+  },
+
+  addListeners: function() {
+    var chuckie = this;
+
+    this.client.addListener('message', function(from, to, text, message) {
+      var regex = new RegExp('^' + chuckie.config.userName + '[,:\s\\|].*$', 'i');
+
+      if ( to === chuckie.config.userName || text.match(regex) ) {
+        chuckie.chuckApi.callApi(function(response) {
+          chuckie.client.say(message.args[0], response);
+        });
+      } 
+    });
   }
-}
+
+};
 
 exports.Chuckie = Chuckie;
